694debce49b2f6d33cf81e372740e5145997b9e0
Vera Petrosyan <76683529+petrosyyaann@users.noreply.github.com>
2023-11-18 14:37:10 +0300

    Update settings.py

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS', default="*")]
 
-CSRF_TRUSTED_ORIGINS=["https://salagupmaslo.ru", "http://salagupmaslo.ru"]
 
 # Application definition
 
